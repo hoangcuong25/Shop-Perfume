432e073d53dd04b877d2c9c0ff80cac925445d5a
hoang cuong <vancuong25112003@gmail.com>
2024-09-30 22:07:16 +0700

    Pagination

---
+++ frontend/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,47 @@
+import { button } from "@material-tailwind/react";
+
+const Pagination = (props) => {
+    const { totalPosts, postPerPage, setCurrentPage, currentPage } = props;
+    let pages = [];
+
+    for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
+        pages.push(i);
+    }
+
+    const prevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1)
+        }
+    }
+
+    const nextPage = () => {
+        if (currentPage < pages.length) {
+            setCurrentPage(currentPage + 1)
+        }
+    }
+
+    return (
+        <div className="mt-16 text-center text-black">
+            <button onClick={() => prevPage()}>
+                Prev
+            </button>
+            {pages.map((page, index) => {
+                return (
+                    <>
+                        <button key={index}
+                            onClick={() => setCurrentPage(page)}
+                            className={currentPage === page ? "mx-7 bg-gray-300 w-10" : "mx-10"}
+                        >
+                            {page}
+                        </button>
+                    </>
+                )
+            })}
+            <button onClick={() => nextPage()}>
+                Next
+            </button>
+        </div>
+    );
+}
+
+export default Pagination;
--- frontend/src/pages/Discovery.jsx
+++ frontend/src/pages/Discovery.jsx
@@ -1,8 +1,18 @@
 import Item from "../components/Item/Item"
 import all_item from "../assets/all_item"
 import banner2 from '../assets/banner2.jpg'
+import { useState } from "react";
+import Pagination from "../components/Pagination/Pagination";
 
 const Discovery = () => {
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [postPerPage, setPostPerPage] = useState(12)
+
+    const lastPostIndex = currentPage * postPerPage
+    const firstPostIndex = lastPostIndex - postPerPage
+    const currentPosts = all_item.slice(firstPostIndex, lastPostIndex)
+
     return (
         <div>
             <div className="bg-gradient-to-r from-cyan-500 to-blue-500 w-[100%] h-[53px] flex justify-center items-center">
@@ -10,10 +20,15 @@ const Discovery = () => {
             </div>
             <img src={banner2} alt="" className="mt-4 px-10" />
             <div className='grid grid-cols-4 mt-7 mx-3'>
-                {all_item.map((item, index) => {
+                {currentPosts.map((item, index) => {
                     return <Item key={index} id={item.id} image={item.image} brands={item.brands} name={item.name} new_price={item.new_price} old_price={item.old_price} />
                 })}
             </div>
+
+            <Pagination
+                totalPosts={all_item.length} postPerPage={postPerPage} setCurrentPage={setCurrentPage}
+                setPostPerPage={setPostPerPage} currentPage={currentPage}
+            />
         </div>
     )
 }
